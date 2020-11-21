e08564f8a0e5a12a0c52d44f044dabb59126b491
goldrunner68 <pilouweb2000@gmail.com>
2020-11-21 08:40:43 +0100

    mise en place

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -6,12 +6,21 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class TestController
 {
 public function index(){
     dd("ca fonctionne");
 }
+
+    /**
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, host="localhost", schemes={"http", "https"})
+     * @param Request $request
+     * @param $age
+     * @return Response
+     */
 
     public function test(Request $request, $age)
     {
